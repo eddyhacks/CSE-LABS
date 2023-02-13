00366e86e7e7bfc4edbbe9cc1bb8e7920e992a4a
aditya <88126377+eddyhacks@users.noreply.github.com>
2023-02-13 10:23:02 +0530

    Create q3.c
    Adding question 3 in lab3

---
+++ PCAP/week3/q3.c
@@ -0,0 +1,48 @@
+#include <mpi/mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argv, char *argc[]){
+	int rank, size, result=0, length,*A;
+
+	MPI_Init(&argv, &argc);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	char string[size * 3], *str;
+	if (rank == 0){
+		printf("Enter the string of length %d : ", size * 3);
+		scanf("%s", string);
+		length = strlen(string);
+		length /= size;
+	}
+
+	MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	str = (char *)calloc(length, sizeof(char));
+	MPI_Scatter(string, length, MPI_CHAR, str, length, MPI_CHAR, 0, MPI_COMM_WORLD);
+	int sendResult = 0;
+	for (int i = 0; i < length; i++){
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+			sendResult++;
+	}
+	sendResult = length - sendResult;
+	A = (int *)calloc(size,sizeof(int));
+
+	printf("Process %d has %d non vowels\n",rank,sendResult);
+	
+	MPI_Gather(&sendResult,1,MPI_INT,A,1,MPI_INT,0,MPI_COMM_WORLD);
+	
+	
+	if (rank == 0){
+		for(int i=0;i<=length;i++){
+			//printf("%d\n",A[i]);
+			result = result + A[i];
+		}
+		printf("The total non-vowels are : %d\n", result);
+	}
+
+	free(str);
+	MPI_Finalize();
+	return 0;
+}
